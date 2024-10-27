3b087fd8a6bb856ff0736d7048d7964b8157f1f3
BYxarek <broekitin@gmail.com>
2024-10-27 22:10:03 +0300

    Delete welcome.php

--- welcome.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-if (!isset($_SESSION['user'])) {
-    header("Location: login.php");
-    exit();
-}
-echo "Authorization/Registration successful! Welcome, " . $_SESSION['user'];
-?>
